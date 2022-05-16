04c9b9e869635bae2f47008a6e62b440d68d1d1f
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-05-16 16:57:40 +0000

    edited code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Zaida Hammel
+// Created on: May 2020
 // This file contains the JS functions for index.html
 
-function myButtonClicked() {
-  // this function displays "Hello, World!"
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+/**
+ * Check servie worker.
+ */
+ if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit6-02-JS/sw.js", {
+    scope: "/ICS2O-Unit6-02-JS/",
+  })
 }
+
+;("use strict")
+/**
+ * This function makes cookie clicker.
+ */
