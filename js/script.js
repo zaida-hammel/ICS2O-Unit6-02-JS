ce800b68785b9678374cbd90fed25fff65604141
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-05-18 16:42:23 -0400

    edited code

--- js/script.js
+++ js/script.js
@@ -15,3 +15,12 @@
 /**
  * This function makes cookie clicker.
  */
+function cookieClicked() {
+  if (localStorage.clicks) {
+    localStorage.clicks = Number(localStorage.clicks) + 1
+    } else {
+      localStorage.clicks = + 1;
+  }
+
+  document.getElementById("answers").innerHTML = "Your clicks: " + localStorage.clicks
+}
